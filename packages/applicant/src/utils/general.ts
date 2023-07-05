af1146256bcf9f653ef4cefc2da21d1cb75e918f
Ryan <ryan.gilbert@cabinetoffice.gov.uk>
2023-07-05 14:04:12 +0100

    Check on equality for env var

--- packages/applicant/src/utils/general.ts
+++ packages/applicant/src/utils/general.ts
@@ -1,5 +1,5 @@
 const getLoginUrl = () => {
-    return process.env.ONE_LOGIN_ENABLED ? process.env.V2_LOGIN_URL : process.env.LOGIN_URL;
+    return process.env.ONE_LOGIN_ENABLED === "true" ? process.env.V2_LOGIN_URL : process.env.LOGIN_URL;
   };
 
     export { getLoginUrl };
