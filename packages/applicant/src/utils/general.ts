111164ac679861d5ab7cdea009ae5f3d516f25bf
Ryan <ryan.gilbert@cabinetoffice.gov.uk>
2023-07-05 15:13:40 +0100

    Fixed styling

--- packages/applicant/src/utils/general.ts
+++ packages/applicant/src/utils/general.ts
@@ -1,5 +1,7 @@
 const getLoginUrl = () => {
-    return process.env.ONE_LOGIN_ENABLED === "enabled" ? process.env.V2_LOGIN_URL! : process.env.LOGIN_URL!;
-  };
+  return process.env.ONE_LOGIN_ENABLED === 'enabled'
+    ? process.env.V2_LOGIN_URL!
+    : process.env.LOGIN_URL!;
+};
 
-    export { getLoginUrl };
+export { getLoginUrl };
