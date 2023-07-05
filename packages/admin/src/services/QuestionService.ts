5db2b4fa765aec8288ac286efd43166f90d2e091
JamieGunnCO <jamie.gunn@cabinetoffice.gov.uk>
2023-07-05 11:45:00 +0100

    fix ts error

--- packages/admin/src/services/QuestionService.ts
+++ packages/admin/src/services/QuestionService.ts
@@ -17,7 +17,7 @@ const patchQuestion = (
     fieldTitle: string;
     displayText: string;
     hintText: string;
-    validation: { [key: string]: object };
+    validation: { mandatory: boolean };
     options: string[];
   }>
 ): Promise<void> => {
