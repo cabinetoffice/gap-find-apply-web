54528ff5ae211a638ec0fbe269de858e296a3f60
Ryan <ryan.gilbert@cabinetoffice.gov.uk>
2023-07-05 11:58:30 +0100

    Added support for v2 logout

--- packages/applicant/src/pages/api/logout.page.tsx
+++ packages/applicant/src/pages/api/logout.page.tsx
@@ -4,5 +4,5 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.redirect(process.env.LOGOUT_URL);
+  res.redirect(process.env.ONE_LOGIN_ENABLED ? process.env.V2_LOGOUT_URL : process.env.LOGOUT_URL);
 }
