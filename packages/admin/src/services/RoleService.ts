cd43a7ff0943a2a7be4f1d6b4ce9d7403e63b8b9
john-tco <john.morley@cabinetoffice.gov.uk>
2023-07-14 16:51:21 +0100

    amend get all roles url

--- packages/admin/src/services/RoleService.ts
+++ packages/admin/src/services/RoleService.ts
@@ -4,7 +4,7 @@ import { axiosSessionConfig } from '../utils/session';
 const BASE_URL = process.env.USER_SERVICE_HOST;
 
 const getAllRoles = async (sessionCookie: string) => {
-  const response = await axios.get(`${BASE_URL}/roles`, axiosSessionConfig(sessionCookie));
+  const response = await axios.get(`${BASE_URL}/role`, axiosSessionConfig(sessionCookie));
   return response.data;
 };
 
