b316ee2f12571058ec40a4ca9a871b0251f12958
john-tco <john.morley@cabinetoffice.gov.uk>
2023-07-31 16:08:16 +0100

    initial

--- packages/admin/src/pages/super-admin-dashboard/index.page.tsx
+++ packages/admin/src/pages/super-admin-dashboard/index.page.tsx
@@ -1,6 +1,11 @@
 import { GetServerSidePropsContext } from 'next';
 import Link from 'next/link';
-import { Button, Checkboxes, Table } from 'gap-web-ui';
+import {
+  Button,
+  Checkboxes,
+  QuestionPageGetServerSideProps,
+  Table,
+} from 'gap-web-ui';
 import Meta from '../../components/layout/Meta';
 import PaginationType from '../../types/Pagination';
 import { getUserTokenFromCookies } from '../../utils/session';
@@ -10,25 +15,40 @@ import { getSuperAdminDashboard } from '../../services/SuperAdminService';
 import { User } from './types';
 import Navigation from './Nagivation';
 import InferProps from '../../types/InferProps';
+import { ButtonTypePropertyEnum } from 'gap-web-ui';
+import getConfig from 'next/config';
 
-export const getServerSideProps = async ({
-  req,
-  query,
-}: GetServerSidePropsContext) => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   const paginationParams: PaginationType = {
     paginate: true,
-    page: Number(query.page || 1) - 1,
-    size: Number(query.limit || 10),
+    page: Number(context.query.page || 1) - 1,
+    size: Number(context.query.limit || 10),
   };
 
-  const userToken = getUserTokenFromCookies(req);
+  const userToken = getUserTokenFromCookies(context.req);
   const { departments, roles, users, userCount } = await getSuperAdminDashboard(
     paginationParams,
     userToken
   );
 
+  const handleRequest = async (formData: FormData) => {
+    console.log({ formData });
+  };
+
+  return QuestionPageGetServerSideProps({
+    context,
+    fetchPageData: await getSuperAdminDashboard(paginationParams, userToken),
+    handleRequest,
+    jwt: getUserTokenFromCookies(context.req),
+    onErrorMessage: 'Failed to filter users, please try again later.',
+    onSuccessRedirectHref: `/super-admin-dashboard/`,
+  });
+
   return {
     props: {
+      // formAction,
       departments,
       roles,
       users,
@@ -58,7 +78,8 @@ const SuperAdminDashboard = ({
   roles,
   users,
   userCount,
-}: InferProps<typeof getServerSideProps>) => {
+}: // formAction,
+InferProps<typeof getServerSideProps>) => {
   return (
     <>
       <Navigation />
@@ -73,41 +94,51 @@ const SuperAdminDashboard = ({
                 <p className="govuk-body">
                   We’ve found <strong>{userCount}</strong> users
                 </p>
-                {/* filter button */}
-                <div className={styles['top-controls']}>
-                  <Button text="Clear all filters" isSecondary />
-                </div>
+                <form
+                  noValidate
+                  method="POST"
+                  // action={formAction}
+                  action={`${process.env.USER_SERVICE_URL}/user/filter`}
+                >
+                  {/* filter button */}
+                  <div className={styles['top-controls']}>
+                    <Button text="Clear all filters" isSecondary />
+                  </div>
 
-                <Checkboxes
-                  questionTitle="Role"
-                  fieldName="role"
-                  titleSize="m"
-                  options={roles
-                    .filter((role) => role.name !== 'FIND')
-                    .map((role) => ({
-                      label: role.label,
-                      value: role.id,
+                  <Checkboxes
+                    questionTitle="Role"
+                    fieldName="role"
+                    titleSize="m"
+                    options={roles
+                      .filter((role) => role.name !== 'FIND')
+                      .map((role) => ({
+                        label: role.label,
+                        value: role.id,
+                      }))}
+                    fieldErrors={[]}
+                    small
+                  />
+
+                  <Checkboxes
+                    questionTitle="Department"
+                    fieldName="department"
+                    titleSize="m"
+                    options={departments.map((department) => ({
+                      label: department.name,
+                      value: department.id,
                     }))}
-                  fieldErrors={[]}
-                  small
-                />
+                    fieldErrors={[]}
+                    small
+                  />
 
-                <Checkboxes
-                  questionTitle="Department"
-                  fieldName="department"
-                  titleSize="m"
-                  options={departments.map((department) => ({
-                    label: department.name,
-                    value: department.id,
-                  }))}
-                  fieldErrors={[]}
-                  small
-                />
-
-                <div className={styles['bottom-controls']}>
-                  <Button text="Apply filters" />
-                  <Button text="Clear all filters" isSecondary />
-                </div>
+                  <div className={styles['bottom-controls']}>
+                    <Button
+                      type={ButtonTypePropertyEnum.Submit}
+                      text="Apply filters"
+                    />
+                    {/* <Button text="Clear all filters" isSecondary /> */}
+                  </div>
+                </form>
               </div>
 
               <div className="govuk-grid-column-two-thirds">
