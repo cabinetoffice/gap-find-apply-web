20a1777eb0b1982330ac674a6fd384be745986d9
john-tco <john.morley@cabinetoffice.gov.uk>
2023-08-04 14:35:12 +0100

    fix copy

--- packages/admin/src/pages/super-admin-dashboard/manage-departments/add/index.page.tsx
+++ packages/admin/src/pages/super-admin-dashboard/manage-departments/add/index.page.tsx
@@ -57,7 +57,7 @@ const AddDepartmentPage = ({
           />
           <TextInput
             questionHintText={
-              'This should be the departments GGIS ID, not the ID of a grant within it.'
+              "This should be the department's GGIS ID, not the ID of a grant within it."
             }
             width="10"
             questionTitle={`GGIS ID number`}
