b668a26dcfcce23e8422537f9c5e81a526e26bfe
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-08-11 14:22:45 +0100

    Add TechnicalSupport navigation bar item

---
+++ packages/admin/src/pages/super-admin-dashboard/Navigation.test.tsx
@@ -0,0 +1,43 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import Navigation from './Navigation';
+import { Role } from './types';
+
+jest.mock('next/config', () => () => ({
+  publicRuntimeConfig: {
+    FIND_A_GRANT_URL: 'mocked-find-a-grant-url',
+    APPLICANT_DOMAIN: 'mocked-applicant-domain',
+    TECHNICAL_SUPPORT_DOMAIN: 'mocked-technical-support-domain',
+  },
+}));
+
+const roles: Role[] = [
+  { name: 'ADMIN', id: '1', description: 'adminRole', label: 'adminLabel' },
+  {
+    name: 'TECHNICAL_SUPPORT',
+    id: '2',
+    description: 'technicalSupportRole',
+    label: 'technicalSupportLabel',
+  },
+];
+
+describe('Navigation', () => {
+  it('renders navigation items correctly when TECHNICAL_SUPPORT role is present in the roles', () => {
+    const { getByText } = render(<Navigation roles={roles} />);
+
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('Manage users')).toBeInTheDocument();
+    expect(getByText('Admin dashboard')).toBeInTheDocument();
+    expect(getByText('Applicant dashboard')).toBeInTheDocument();
+    expect(getByText('Technical Support dashboard')).toBeInTheDocument();
+  });
+
+  it('does not render "Technical Support dashboard" link for non-TECHNICAL_SUPPORT roles', () => {
+    const { queryByText } = render(<Navigation roles={roles.slice(0, 1)} />);
+
+    const technicalSupportDashboardLink = queryByText(
+      'Technical Support dashboard'
+    );
+    expect(technicalSupportDashboardLink).toBeNull();
+  });
+});
