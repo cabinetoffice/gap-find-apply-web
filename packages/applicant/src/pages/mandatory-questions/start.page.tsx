1faf1ab0bfe2cc0b6fde7490d091a25d7a418cc9
conor <conor.fayle@and.digital>
2023-10-11 16:09:09 +0100

    TMI2-353|add start page

--- packages/applicant/src/pages/mandatory-questions/start.page.tsx
+++ packages/applicant/src/pages/mandatory-questions/start.page.tsx
@@ -1,17 +1,45 @@
+import { GetServerSideProps } from 'next';
+import { routes } from '../../utils/routes';
 import Layout from '../../components/partials/Layout';
 import Meta from '../../components/partials/Meta';
-import { routes } from '../../utils/routes';
+import { Button } from 'gap-web-ui';
 
-const FirstMandatoryQuestion = () => {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  return { props: {} };
+};
+
+const MandatoryQuestionsBeforeYouStart = () => {
   return (
     <>
-      <Meta title="View my applications - Apply for a grant" />
-
-      <Layout backBtnUrl={routes.dashboard}>
-        <>NEW PAGE</>
+      <Meta title="Before you start" />
+      <Layout backBtnUrl={routes.organisation.index}>
+        <h1 className="govuk-heading-l">Before you start</h1>
+        <p className="govuk-body">
+          Before you start, we’d like to ask you a few questions.
+        </p>
+        <p className="govuk-body">
+          Before you start, we’d like to ask you a few questions.
+        </p>
+        <p className="govuk-body">
+          These questions will be used by the grant’s administrators to prevent
+          fraud and help Find a grant understand the demand for this grant.
+        </p>
+        <p className="govuk-body">You will need:</p>
+        <ul className="govuk-list govuk-list--bullet">
+          <li data-cy="cy-before-you-start-help-text-bullet-1">
+            your organisation’s address
+          </li>
+          <li data-cy="cy-before-you-start-help-text-bullet-2">
+            your Companies House number (if you have one)
+          </li>
+          <li data-cy="cy-before-you-start-help-text-bullet-3">
+            your Charity Commission number (if you have one)
+          </li>
+        </ul>
+        <Button text="Continue" />
       </Layout>
     </>
   );
 };
 
-export default FirstMandatoryQuestion;
+export default MandatoryQuestionsBeforeYouStart;
