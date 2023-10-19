a07054100c8470be7fbc69a9c13130737b11391c
kiramarstonTCO <107405237+kiramarstonTCO@users.noreply.github.com>
2023-10-19 10:00:35 +0100

    GAP-2231 - Added unpublishing content and layout formatting (#109)

--- packages/admin/src/pages/build-application/[applicationId]/components/UnpublishSummary.tsx
+++ packages/admin/src/pages/build-application/[applicationId]/components/UnpublishSummary.tsx
@@ -19,39 +19,44 @@ const UnpublishSummary = ({
         Add this application form to your grant advert?
       </h2>
 
-      <p className="govuk-body">
-        To add this application form to your advert, copy the URL below and
-        paste it into the &apos;How to Apply&apos; section of your grant advert.
-      </p>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-two-thirds">
+          <p className="govuk-body">
+            To add this application form to your advert, copy the URL below and
+            paste it into the &apos;How to Apply&apos; section of your grant
+            advert.
+          </p>
 
-      <p className="govuk-body break-all-words">
-        <a href={linkToApplicantApplicationForm} className="govuk-link">
-          {linkToApplicantApplicationForm}
-        </a>
-      </p>
+          <p className="govuk-body break-all-words">
+            <a href={linkToApplicantApplicationForm} className="govuk-link">
+              {linkToApplicantApplicationForm}
+            </a>
+          </p>
 
-      <hr className="govuk-section-break govuk-section-break--l" />
+          <hr className="govuk-section-break govuk-section-break--l" />
 
-      <h2 className="govuk-heading-s govuk-!-margin-bottom-2">
-        Unpublish this application form?
-      </h2>
+          <h2 className="govuk-heading-s govuk-!-margin-bottom-2">
+            Unpublish this application form?
+          </h2>
 
-      <p className="govuk-body">
-        Once unpublished, your application form will no longer appear on{' '}
-        {findAGrantLink}. You will be able to edit the application form and
-        re-publish.
-      </p>
+          <p className="govuk-body">
+            Once unpublished, your application form will no longer appear on{' '}
+            {findAGrantLink} and applications cannot be submitted. You will be
+            able to edit the application form and re-publish.
+          </p>
 
-      <div className="govuk-button-group">
-        <CustomLink
-          isButton
-          href={`/build-application/${applicationId}/unpublish-confirmation`}
-          dataCy="cy_unpublishApplication-button"
-        >
-          Unpublish
-        </CustomLink>
+          <div className="govuk-button-group">
+            <CustomLink
+              isButton
+              href={`/build-application/${applicationId}/unpublish-confirmation`}
+              dataCy="cy_unpublishApplication-button"
+            >
+              Unpublish
+            </CustomLink>
 
-        <CustomLink href={`/scheme/${grantSchemeId}`}>Exit</CustomLink>
+            <CustomLink href={`/scheme/${grantSchemeId}`}>Exit</CustomLink>
+          </div>
+        </div>
       </div>
     </>
   );
