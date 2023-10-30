cffa3ecc700bf572862da38b50c71674b6ee7577
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-10-30 17:25:45 +0000

    Further improvements

--- jest.config.mjs
+++ jest.config.mjs
@@ -34,6 +34,14 @@ const config = {
     },
     {
       displayName: 'gap-web-ui',
+      rootDir: './packages/gap-web-ui',
+      testEnvironment: 'jest-environment-jsdom',
+      moduleNameMapper: {
+        //https://jestjs.io/docs/webpack#handling-static-assets//
+        '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+        '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+        clearMocks: true,
+      },
     },
   ],
 };
