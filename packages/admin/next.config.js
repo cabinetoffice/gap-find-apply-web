1432c29af0e17122adc076454d7486322526b0e7
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-11-02 16:31:36 +0000

    Adding outputFileTracingRoot next config

--- packages/admin/next.config.js
+++ packages/admin/next.config.js
@@ -13,7 +13,8 @@ module.exports = {
   reactStrictMode: true,
   // experimental: { optimizeCss: true },
   output: 'standalone',
-
+  // this includes files from the monorepo base two directories up
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   /* Add Your Scss File Folder Path Here */
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
