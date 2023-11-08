4f0f41d98b3cf0abe132cce1ee09c65f8d8ccd33
Ryan <ryan.gilbert@cabinetoffice.gov.uk>
2023-11-08 09:53:42 +0000

    Fix app wide formatting issue

--- packages/admin/src/pages/super-admin-dashboard/user/[id]/change-owner.page.tsx
+++ packages/admin/src/pages/super-admin-dashboard/user/[id]/change-owner.page.tsx
@@ -1,3 +1,4 @@
+import { Button, FlexibleQuestionPageLayout, TextInput } from 'gap-web-ui';
 import CustomLink from '../../../../components/custom-link/CustomLink';
 import Meta from '../../../../components/layout/Meta';
 import InferProps from '../../../../types/InferProps';
@@ -7,6 +8,11 @@ export async function getServerSideProps() {
     props: {
       fieldErrors: [],
       user: { gapUserId: 1 },
+      grantName: 'Grant Name',
+      schemeId: 1,
+      defaultValue: '',
+      formAction: '',
+      csrfToken: '',
     },
   };
 }
@@ -14,6 +20,11 @@ export async function getServerSideProps() {
 const ChangeOwnerPage = ({
   fieldErrors,
   user,
+  grantName,
+  schemeId,
+  defaultValue,
+  formAction,
+  csrfToken,
 }: InferProps<typeof getServerSideProps>) => {
   return (
     <>
@@ -27,8 +38,30 @@ const ChangeOwnerPage = ({
         isBackButton
         href={`/super-admin-dashboard/user/${user.gapUserId}`}
       />
-
-      <h1 className="govuk-heading-l">TODO change owner page</h1>
+      <div className="govuk-!-padding-top-7">
+        <FlexibleQuestionPageLayout
+          formAction={formAction}
+          fieldErrors={fieldErrors}
+          csrfToken={csrfToken}
+        >
+          <span className="govuk-caption-xl">{grantName}</span>
+          <h1 className="govuk-heading-l">Select a new owner</h1>
+          <p className="govuk-body">
+            You can select any admin account on the Find a grant service as a
+            new owner for this grant. The new owner will be able to view, edit,
+            and delete this grant. The previous owner will no longer be able to
+            access this grant.
+          </p>
+          <TextInput
+            questionTitle="New owner's email address"
+            fieldName="emailAdress"
+            fieldErrors={fieldErrors}
+            defaultValue={defaultValue}
+            textInputSubtype="email"
+          />
+          <Button text="Continue" />
+        </FlexibleQuestionPageLayout>
+      </div>
     </>
   );
 };
