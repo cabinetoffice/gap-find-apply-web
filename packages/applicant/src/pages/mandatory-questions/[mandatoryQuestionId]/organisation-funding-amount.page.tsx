af8f3e2d59d7993d83b905f370bd878b49c74d23
kiramarstonTCO <kira.marston@cabinetoffice.gov.uk>
2023-11-17 12:08:26 +0000

    TMI2-332 - Removed styling div

--- packages/applicant/src/pages/mandatory-questions/[mandatoryQuestionId]/organisation-funding-amount.page.tsx
+++ packages/applicant/src/pages/mandatory-questions/[mandatoryQuestionId]/organisation-funding-amount.page.tsx
@@ -8,7 +8,6 @@ import {
 import Layout from '../../../components/partials/Layout';
 import Meta from '../../../components/partials/Meta';
 import InferProps from '../../../types/InferProps';
-import { routes } from '../../../utils/routes';
 import getServerSideProps from './getServerSideProps';
 
 export { getServerSideProps };
