86a037b41d19cefe3e496304e3410a4e60b90640
kiramarstonTCO <107405237+kiramarstonTCO@users.noreply.github.com>
2023-11-29 16:13:59 +0000

    TMI2-421 - Added download link for checks that cannot be sent to Spotlight (#188)
    * TMI2-421 - Added download link for checks that cannot be sent to Spotlight
    
    * download validation errors
    
    * fixed download href
    
    * TMI2-421 - Added missing tests for Spotlight errors
    
    ---------
    
    Co-authored-by: rachelswart <rachel.swart@cabinetoffice.gov.uk>
    Co-authored-by: Jim Purvis <james.purvis@and.digital>

---
+++ packages/admin/src/pages/api/downloadSpotlightValidationErrorSubmissions.page.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSpotlightValidationErrorSubmissions } from '../../services/SpotlightSubmissionService';
+import { getSessionIdFromCookies } from '../../utils/session';
+
+const downloadSpotlightValidationErrorSubmissions = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const schemeId = req.query.schemeId as string;
+  const errorRedirect = () => {
+    res.redirect(
+      `${process.env.SUB_PATH}/service-error?serviceErrorProps=${JSON.stringify(
+        {
+          errorInformation:
+            'Something went wrong while trying to download the information for Spotlight submissions with validation errors.',
+          linkAttributes: {
+            href: `/scheme/${schemeId}/manage-due-diligence-checks`,
+            linkText: 'Please return',
+            linkInformation: ' and try again.',
+          },
+        }
+      )}`
+    );
+  };
+
+  let result;
+  try {
+    result = await getSpotlightValidationErrorSubmissions(
+      getSessionIdFromCookies(req),
+      schemeId
+    );
+  } catch (error) {
+    errorRedirect();
+    return;
+  }
+
+  if (result !== undefined) {
+    res.setHeader('Content-Disposition', result.headers['content-disposition']);
+    res.setHeader('Content-Type', result.headers['content-type']);
+    res.setHeader('Content-Length', result.headers['content-length']);
+    res.send(result.data);
+  } else {
+    errorRedirect();
+    return;
+  }
+};
+
+export default downloadSpotlightValidationErrorSubmissions;
