e07b03fe0f95dfd858d2f54cab38810e5485daa6
JamieGunnCO <jamie.gunn@cabinetoffice.gov.uk>
2023-12-12 14:24:03 +0000

    remove unused code

--- packages/admin/postinstall.js
+++ packages/admin/postinstall.js
@@ -4,10 +4,14 @@ const path = require('path');
 // tinymce doesn't work well when bundled, so we're copying it to the public dir
 // so nextjs will self-host the dependency
 
+const copyFiles = async (sourceDir, targetDir) => {
+  await fse.emptyDir(targetDir);
+  await fse.copy(sourceDir, targetDir);
+};
+
 // use require.resolve to get path of tinymce install, since the yarn workspaces
 // mean this can move around
 const sourceDir = path.dirname(require.resolve('tinymce'));
 const targetDir = path.join(__dirname, 'public', 'tinymce');
 
-fse.emptyDirSync(targetDir);
-fse.copySync(sourceDir, targetDir);
+copyFiles(sourceDir, targetDir);
