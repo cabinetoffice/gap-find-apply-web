f37b3c3f6407f495ebc100d44d82c5e92088f6df
rachelswart <99667350+rachelswart@users.noreply.github.com>
2024-01-03 11:01:10 +0000

    change root background colour to match footer colour (#223)

--- packages/admin/src/pages/_document.page.tsx
+++ packages/admin/src/pages/_document.page.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html className="root-background">
       <Head />
       <body className="govuk-template__body js-enabled">
         <Main />
