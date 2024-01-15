a6000ccfdf5d41587b039b6f3cb65c5cc641945f
john-tco <john.morley@cabinetoffice.gov.uk>
2024-01-15 17:15:58 +0000

    use arrayBuffer

--- packages/applicant/src/pages/api/routes/submissions/[submissionId]/download-summary.page.tsx
+++ packages/applicant/src/pages/api/routes/submissions/[submissionId]/download-summary.page.tsx
@@ -9,5 +9,8 @@ export default async function handler(
 ) {
   const submissionId = req.query.submissionId.toString();
   const { data } = await downloadSummary(submissionId, getJwtFromCookies(req));
+
+  res.setHeader('Content-Type', 'application/octet-stream');
+  res.setHeader('Content-Disposition', 'attachment; filename=submission.odt');
   res.send(Buffer.from(data));
 }
