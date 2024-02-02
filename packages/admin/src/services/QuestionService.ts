9f7b318c5fe5b9b6efb18772a064b61062318fd1
john-tco <john.morley@cabinetoffice.gov.uk>
2024-02-02 10:35:31 +0000

    fix tests

--- packages/admin/src/services/QuestionService.ts
+++ packages/admin/src/services/QuestionService.ts
@@ -39,7 +39,7 @@ const postQuestion = (
     hintText?: string;
     displayText?: string;
     questionSuffix?: string;
-    validation: { maxWords: number; mandatory: boolean };
+    validation: { maxWords?: number; mandatory: boolean };
     options?: string[];
   }
 ): Promise<void> => {
