bdfecd6b7e58243375c36a7b2cb675719a91753a
paul-lawlor-tco <145372589+paul-lawlor-tco@users.noreply.github.com>
2024-02-06 13:40:02 +0000

    Update packages/applicant/src/pages/submissions/[submissionId]/summary.page.tsx
    Co-authored-by: john-tco <135222889+john-tco@users.noreply.github.com>
    Signed-off-by: paul-lawlor-tco <145372589+paul-lawlor-tco@users.noreply.github.com>

--- packages/applicant/src/pages/submissions/[submissionId]/summary.page.tsx
+++ packages/applicant/src/pages/submissions/[submissionId]/summary.page.tsx
@@ -140,14 +140,11 @@ export default function SubmissionSummary({
               }
               method="POST"
             >
-              {closedAndInProgress ? (
+              {closedAndInProgress && (
                 <ImportantBanner
                   bannerHeading="This grant has closed. You cannot submit your application"
                   bannerContent="You can still view your answers and download a copy of your application on this page."
-                />
-              ) : (
-                <></>
-              )}
+                />)}
               <span
                 className="govuk-caption-l"
                 data-cy={`cy-application-name-${applicationName}`}
