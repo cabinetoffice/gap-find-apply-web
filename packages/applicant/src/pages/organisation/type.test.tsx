ee0455021aa7f7a0cd0bbd70deb9fb59f2f5c7cc
kiramarstonTCO <kira.marston@cabinetoffice.gov.uk>
2024-02-16 11:59:46 +0000

    TMI2-628 - adds local authority to applicant organisation options

--- packages/applicant/src/pages/organisation/type.test.tsx
+++ packages/applicant/src/pages/organisation/type.test.tsx
@@ -12,6 +12,7 @@ describe('Organisation type page', () => {
     csrfToken: 'testCSRFToken',
     formAction: 'testFormAction',
     organisationId: 'testOrganisationId',
+    organisationType: 'testOrganisationType',
     defaultFields: {
       type: '',
     },
@@ -53,6 +54,10 @@ describe('Organisation type page', () => {
       name: 'I am applying as an individual',
       checked: false,
     });
+    screen.getByRole('radio', {
+      name: 'Local authority',
+      checked: false,
+    });
     screen.getByRole('radio', { name: 'Other', checked: false });
   });
 
@@ -75,6 +80,10 @@ describe('Organisation type page', () => {
     screen.getByRole('radio', { name: 'Charity', checked: true });
     screen.getByRole('radio', {
       name: 'I am applying as an individual',
+      checked: false,
+    });
+    screen.getByRole('radio', {
+      name: 'Local authority',
       checked: false,
     });
     screen.getByRole('radio', { name: 'Other', checked: false });
