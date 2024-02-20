6db6793687a58f78fb707389dee1e159af37c669
dominicwest <dominic.west@cabinetoffice.gov.uk>
2024-02-20 16:31:07 +0000

    Generic service error & 404 pages

---
+++ packages/admin/src/app/error.page.tsx
@@ -0,0 +1,31 @@
+'use client'; // Error components must be Client Components
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Sorry, there is a problem with the service – Find a Grant – GOV.UK',
+};
+
+export default function Error() {
+  return (
+    <div className="govuk-grid-row">
+      <div className="govuk-grid-column-two-thirds">
+        <h1 className="govuk-heading-l">
+          Sorry, there is a problem with the service
+        </h1>
+
+        <p className="govuk-body">Try again later.</p>
+
+        <p className="govuk-body">
+          If the issue persists, contact our support team at{' '}
+          <a
+            href="mailto:findagrant@cabinetoffice.gov.uk"
+            className="govuk-link"
+          >
+            findagrant@cabinetoffice.gov.uk
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
