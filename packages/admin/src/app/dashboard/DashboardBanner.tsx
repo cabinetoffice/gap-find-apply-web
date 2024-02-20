df5fc2c8a2172bfd7e9f12fc1d6445a31d0b3b69
dominicwest <dominic.west@cabinetoffice.gov.uk>
2024-02-20 12:30:54 +0000

    Refactor horrendous dashboard banner logic

--- packages/admin/src/app/dashboard/DashboardBanner.tsx
+++ packages/admin/src/app/dashboard/DashboardBanner.tsx
@@ -2,49 +2,36 @@ import { ImportantBanner } from 'gap-web-ui';
 import { DashboardPageProps } from './page.page';
 
 export default function DashboardBanner({ searchParams }: DashboardPageProps) {
-  const FAILED = 'FAILED';
-  const SUCCEEDED = 'SUCCEEDED';
+  if (process.env.ONE_LOGIN_MIGRATION_JOURNEY_ENABLED !== 'true') return;
 
-  const getBannerProps = (searchParams: DashboardPageProps['searchParams']) => {
-    if (process.env.ONE_LOGIN_MIGRATION_JOURNEY_ENABLED !== 'true') return null;
-    const statusArray = Object.values(searchParams);
-    if (statusArray.includes(FAILED)) return FAILED;
-    if (statusArray.includes(SUCCEEDED))
-      return {
-        bannerHeading:
-          'Your data has been successfully added to your One Login account.',
-        isSuccess: true,
-      };
+  const statusArray = Object.values(searchParams);
+  if (statusArray.includes('FAILED')) {
+    return (
+      <ImportantBanner
+        bannerHeading="Something went wrong while transferring your data."
+        bannerContent={
+          <p className="govuk-body">
+            Please get in contact with our support team at{' '}
+            <a
+              className="govuk-notification-banner__link"
+              href="mailto:findagrant@cabinetoffice.gov.uk"
+            >
+              findagrant@cabinetoffice.gov.uk
+            </a>
+            {'.'}
+          </p>
+        }
+        isSuccess={false}
+      />
+    );
+  }
 
-    return null;
-  };
-
-  const bannerProps = getBannerProps(searchParams);
-
-  const errorBannerProps = {
-    bannerHeading: 'Something went wrong while transferring your data.',
-    bannerContent: (
-      <p className="govuk-body">
-        Please get in contact with our support team at{' '}
-        <a
-          className="govuk-notification-banner__link"
-          href="mailto:findagrant@cabinetoffice.gov.uk"
-        >
-          findagrant@cabinetoffice.gov.uk
-        </a>
-        {'.'}
-      </p>
-    ),
-    isSuccess: false,
-  };
-
-  return (
-    <>
-      {bannerProps && (
-        <ImportantBanner
-          {...(bannerProps === FAILED ? errorBannerProps : bannerProps)}
-        />
-      )}
-    </>
-  );
+  if (statusArray.includes('SUCCEEDED')) {
+    return (
+      <ImportantBanner
+        bannerHeading="Your data has been successfully added to your One Login account."
+        isSuccess={true}
+      />
+    );
+  }
 }
