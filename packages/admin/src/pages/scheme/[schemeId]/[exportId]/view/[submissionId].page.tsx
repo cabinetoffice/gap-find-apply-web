070d1bad74a420b3f6debbf05493205acd75be2b
kiramarstonTCO <kira.marston@cabinetoffice.gov.uk>
2024-02-29 11:57:45 +0000

    TMI2-669 - Added conditional logic for rendering option to download attachments

--- packages/admin/src/pages/scheme/[schemeId]/[exportId]/view/[submissionId].page.tsx
+++ packages/admin/src/pages/scheme/[schemeId]/[exportId]/view/[submissionId].page.tsx
@@ -32,6 +32,7 @@ export const getServerSideProps = async ({
       sessionCookie
     );
   } catch (error) {
+    console.log(error);
     return errorPageRedirect;
   }
 
@@ -95,6 +96,27 @@ const SubmissionSummary = ({
           })}
         </div>
       </div>
+
+      {submission.attachmentsZipLocation && (
+        <>
+          <h2 className="govuk-heading-m">Download attachments</h2>
+          <p className="govuk-body">
+            You can{' '}
+            <CustomLink
+              href={`/apply/admin/api/signed-url?key=${encodeURIComponent(
+                submission.attachmentsZipLocation
+              )}`}
+              ariaLabel={`Download submission "${submission.name}"`}
+              excludeSubPath
+            >
+              download a copy of any files attached to this application (ZIP).
+            </CustomLink>
+          </p>
+          <CustomLink href={backButtonHref} isSecondaryButton>
+            Return to overview
+          </CustomLink>
+        </>
+      )}
     </>
   );
 };
