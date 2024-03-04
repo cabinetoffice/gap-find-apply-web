e56047f6e2fa66f1f0c2d4d0f85bba398b771332
jimpurvisTCO <151734116+jimpurvisTCO@users.noreply.github.com>
2024-03-04 10:16:13 +0000

    TMI2-600: Download all applications / Error in export (#353)
    * TMI2-602, TMI2-603, TMI2-604, TMI2-617: Download all applications  (#323)
    
    * TMI2-602 - Initial commit of updated page
    
    * TMI2-602 - Adding unit tests
    
    * TMI2-602 - Restructuring page to accommodate for all journeys
    
    * TMI2-603, TMI-604 - Adding error banner and placeholders for pagination
    
    * TMI2-603 - Tidying up
    
    ---------
    
    Co-authored-by: Jim Purvis <james.purvis@and.digital>
    
    * TMI2-633: Functional 'Download all' button (#340)
    
    * TMI2-602 - Initial commit of updated page
    
    * TMI2-602 - Adding unit tests
    
    * TMI2-602 - Restructuring page to accommodate for all journeys
    
    * TMI2-603, TMI-604 - Adding error banner and placeholders for pagination
    
    * TMI2-603 - Tidying up
    
    * TMI2-633 - Adding functionality to Download All button
    
    * TMI2-633 - Removing console.log
    
    ---------
    
    Co-authored-by: Jim Purvis <james.purvis@and.digital>
    
    * TMI2-603-download-individual-applications-success-path (#346)
    
    * TMI2-602 - Initial commit of updated page
    
    * TMI2-602 - Adding unit tests
    
    * TMI2-602 - Restructuring page to accommodate for all journeys
    
    * TMI2-603, TMI-604 - Adding error banner and placeholders for pagination
    
    * TMI2-603 - Tidying up
    
    * Add individual download page
    
    * Update pagination component
    
    * Add back button url
    
    ---------
    
    Co-authored-by: Jim Purvis <james.purvis@and.digital>
    Co-authored-by: jimpurvisTCO <151734116+jimpurvisTCO@users.noreply.github.com>
    
    * TMI2-600 restructure (#351)
    
    * TMI2-600 - Restructuring and moving 'Download all submissions' page to meet new API spec
    
    * TMI2-600 - Connecting up to new export details API
    
    * TMI2-600 - Adding pagination (still to be linked to view page)
    
    ---------
    
    Co-authored-by: Jim Purvis <james.purvis@and.digital>
    
    * TMI2-600 - Only requesting necessary exports for each
    
    * TMI2-600 - Unit tests
    
    * TMI2-603: Update download-individual page
    
    * Integrate API to get submissions
    * Implement pagination
    * Remove redundant code
    
    * TMI2-603: Add unit tests for download-individual page
    
    * TMI2-605 -  View submission summary (error path) (#352)
    
    * TMI2-605 - View read-only summary of application that could not be downloaded
    
    * TMI2-605 - Added link to view individual read-only submissions
    
    * TMI2-605 - Linking frontend to new failed export details endpoint
    
    ---------
    
    Co-authored-by: Jim Purvis <james.purvis@and.digital>
    
    * TMI2-600 - fix for use of an 'any' in submissions list
    
    * Visual tweaks
    
    * TMI2-600 - typo fix
    
    * Clean up code
    
    * TMI2-669 - download attachments error path (#356)
    
    * TMI2-669 - Added conditional logic for rendering option to download attachments
    
    * TMI2-669 - Added tests
    
    * Update DownloadMessage component
    
    * Component now refers to plural applications if there is more than 1
    
    * Update download-individual page
    
    * Pagination now only displays margin when there is more than 1 page
    * Item type is now set to applications
    
    * TMI2-600 - PR comments re: view application
    
    * TMI2-600 - Unit test update
    
    * Update index.page.tsx and download-individual.page.tsx
    
    * Replace DownloadMessage banner with ImportantBanner
    * Align text with Figma designs
    * Make pagination more readable
    * Remove unnecessary components
    
    ---------
    
    Co-authored-by: Jim Purvis <james.purvis@and.digital>
    Co-authored-by: Iain Cooper <iain.cooper@cabinetoffice.gov.uk>
    Co-authored-by: IlyasBaqqari-CabinetOffice <127317407+IlyasBaqqari-CabinetOffice@users.noreply.github.com>
    Co-authored-by: IlyasBaqqari-CabinetOffice <ilyas.baqqari@cabinetoffice.gov.uk>
    Co-authored-by: kiramarstonTCO <107405237+kiramarstonTCO@users.noreply.github.com>

---
+++ packages/admin/src/pages/scheme/[schemeId]/[exportId]/index.page.tsx
@@ -0,0 +1,231 @@
+import { ImportantBanner, Table } from 'gap-web-ui';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import React from 'react';
+
+import CustomLink from '../../../../components/custom-link/CustomLink';
+import {
+  ExportDetails,
+  getExportDetails,
+} from '../../../../services/ExportService';
+import { getGrantScheme } from '../../../../services/SchemeService';
+import { generateErrorPageRedirect } from '../../../../utils/serviceErrorHelpers';
+import { getSessionIdFromCookies } from '../../../../utils/session';
+import { TheadColumn } from 'gap-web-ui/dist/cjs/components/table/Table';
+import { Pagination } from '../../../../components/pagination/Pagination';
+import Meta from '../../../../components/layout/Meta';
+
+export const getServerSideProps = async ({
+  req,
+  query,
+  params,
+}: GetServerSidePropsContext) => {
+  const sessionCookie = getSessionIdFromCookies(req);
+  const { schemeId, exportId } = params as Record<string, string>;
+
+  const pagination = {
+    paginate: true,
+    page: Number(query.page ?? 1) - 1,
+    size: Number(query.limit ?? 10),
+  };
+
+  let grantScheme;
+  let unavailableSubmissions = [] as ExportDetails[];
+  let availableSubmissionsTotalCount = 0;
+  let unavailableSubmissionsTotalCount = 0;
+  let superZipLocation = '';
+
+  try {
+    grantScheme = await getGrantScheme(schemeId, sessionCookie);
+
+    const successExportDetails = await getExportDetails(
+      exportId,
+      false,
+      pagination,
+      sessionCookie
+    );
+
+    availableSubmissionsTotalCount = successExportDetails.successCount;
+    unavailableSubmissionsTotalCount = successExportDetails.failedCount;
+    superZipLocation = successExportDetails.superZipFileLocation;
+
+    if (unavailableSubmissionsTotalCount > 0) {
+      const failedExportDetails = await getExportDetails(
+        exportId,
+        true,
+        pagination,
+        sessionCookie
+      );
+      unavailableSubmissions = failedExportDetails.exportedSubmissions;
+    }
+  } catch (err) {
+    return generateErrorPageRedirect(
+      'Something went wrong while trying to view submission applications.',
+      '/dashboard'
+    );
+  }
+
+  if (
+    availableSubmissionsTotalCount == 0 &&
+    unavailableSubmissionsTotalCount == 0
+  )
+    return generateErrorPageRedirect(
+      'There are no submissions available to view or download.',
+      '/dashboard'
+    );
+
+  return {
+    props: {
+      individualApplicationsHref: `/scheme/${schemeId}/${exportId}/download-individual`,
+      superZipLocation: superZipLocation,
+      schemeName: grantScheme.name,
+      availableSubmissionsTotalCount: availableSubmissionsTotalCount,
+      unavailableSubmissionsTotalCount: unavailableSubmissionsTotalCount,
+      unavailableSubmissions: unavailableSubmissions,
+      schemeId: schemeId,
+      exportId: exportId,
+    },
+  };
+};
+
+export const CompletedSubmissions = ({
+  schemeName,
+  individualApplicationsHref,
+  availableSubmissionsTotalCount,
+  unavailableSubmissionsTotalCount,
+  unavailableSubmissions,
+  superZipLocation,
+  schemeId,
+  exportId,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const tableHeadColumns = [
+    {
+      name: <>{true && 'Organisation'}</>,
+      width: 'three-quarters',
+    },
+    {
+      name: 'Action',
+      isVisuallyHidden: true,
+    },
+  ] as TheadColumn[];
+
+  const tableRows = unavailableSubmissions.map((submission) => {
+    return {
+      cells: [
+        {
+          content: <>{true && submission.name}</>,
+        },
+        {
+          content: (
+            <div className="govuk-!-text-align-right">
+              <CustomLink
+                href={`/scheme/${schemeId}/${exportId}/view/${submission.submissionId}`}
+                ariaLabel={`View submission "${submission.name}"`}
+              >
+                View
+              </CustomLink>
+            </div>
+          ),
+        },
+      ],
+    };
+  });
+
+  return (
+    <>
+      <Meta title={`Download applications - Manage a grant`} />
+      <div className="govuk-grid-row govuk-!-padding-top-7 govuk-!-margin-bottom-6">
+        <div className="govuk-grid-column-two-thirds">
+          {unavailableSubmissionsTotalCount > 0 && (
+            <ImportantBanner
+              bannerHeading={`Cannot download ${unavailableSubmissionsTotalCount} ${
+                unavailableSubmissionsTotalCount > 1
+                  ? 'applications'
+                  : 'application'
+              }`}
+              bannerContent={
+                'You can view a read-only copy of the applications that are affected in the "Applications unavailable for download" section of this page.'
+              }
+            />
+          )}
+
+          <h1 className="govuk-heading-l">{schemeName}</h1>
+
+          <h2 className="govuk-heading-m">
+            Applications available to download
+          </h2>
+
+          <p className="govuk-body">
+            Your grant has{' '}
+            <b>
+              {availableSubmissionsTotalCount}{' '}
+              {availableSubmissionsTotalCount === 1
+                ? 'application'
+                : 'applications'}
+            </b>{' '}
+            available to download.
+          </p>
+          <div className="govuk-button-group">
+            <CustomLink
+              href={`/apply/admin/api/signed-url?key=${encodeURIComponent(
+                superZipLocation
+              )}`}
+              isButton
+              excludeSubPath
+              disabled={availableSubmissionsTotalCount == 0}
+            >
+              Download all applications
+            </CustomLink>
+
+            <CustomLink
+              href={individualApplicationsHref}
+              isSecondaryButton
+              disabled={availableSubmissionsTotalCount == 0}
+            >
+              View individual applications
+            </CustomLink>
+          </div>
+
+          {unavailableSubmissions.length > 0 && (
+            <>
+              <hr className="govuk-section-break govuk-section-break--visible govuk-section-break--m govuk-!-margin-top-2 govuk-!-margin-bottom-7"></hr>
+              <h2 className="govuk-heading-m">
+                Applications unavailable for download
+              </h2>
+
+              <p className="govuk-body">
+                Your grant has{' '}
+                <b>
+                  {unavailableSubmissionsTotalCount}{' '}
+                  {unavailableSubmissionsTotalCount === 1
+                    ? 'application'
+                    : 'applications'}
+                </b>{' '}
+                that cannot be downloaded. You can still view a read-only
+                version of these applications.
+              </p>
+
+              <div className="submissions-download-table">
+                <Table
+                  tableClassName="table-thead-bottom-border"
+                  captionSize="m"
+                  tHeadColumns={tableHeadColumns}
+                  rows={tableRows}
+                />
+
+                <Pagination
+                  additionalQueryData={{}}
+                  itemsPerPage={10}
+                  totalItems={unavailableSubmissionsTotalCount}
+                  itemType="applications"
+                  itemCountMargin={unavailableSubmissionsTotalCount > 10}
+                />
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CompletedSubmissions;
