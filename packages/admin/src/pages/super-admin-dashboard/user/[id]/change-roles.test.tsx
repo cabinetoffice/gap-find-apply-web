f493d07163d7421843632975c055322999dfa9fb
paul-lawlor-tco <paul.lawlor1@cabinetoffice.gov.uk>
2024-03-11 11:44:37 +0000

    Fix unit tests and add cases for 'Change' button URL when a SA views a users details.

--- packages/admin/src/pages/super-admin-dashboard/user/[id]/change-roles.test.tsx
+++ packages/admin/src/pages/super-admin-dashboard/user/[id]/change-roles.test.tsx
@@ -128,7 +128,8 @@ describe('Render edit role page', () => {
   test('SHOULD NOT disable the Administrator checkbox if the user DOES NOT own a scheme', () => {
     render(componentNonOwner);
     expect(
-      screen.getByText('Administrator').parentElement?.previousSibling
+      screen.getByText('Administrator').parentElement?.parentElement
+        ?.parentElement
     ).not.toBeDisabled();
   });
   test('SHOULD disable the Administrator checkbox if the user DOES own a scheme', () => {
