24ab522c3ed7a7b038b70477cbe82d00be0400d2
dominicwest <dominic.west@cabinetoffice.gov.uk>
2024-03-12 14:52:34 +0000

    GAP-2502: Removing unneeded div

--- packages/admin/src/pages/build-application/[applicationId]/[sectionId]/index/QuestionRowActionComponent.tsx
+++ packages/admin/src/pages/build-application/[applicationId]/[sectionId]/index/QuestionRowActionComponent.tsx
@@ -26,37 +26,35 @@ const QuestionRowActionComponent = ({
 }: QuestionRowActionComponentProps) => {
   return (
     <div className="govuk-!-text-align-right govuk-!-padding-top-4">
-      <div className="">
-        <button
-          className={`govuk-button govuk-button--secondary govuk-!-margin-right-2 govuk-!-margin-bottom-0 ${styles['button']}`}
-          data-module="govuk-button"
-          data-cy="cyUpButton"
-          name={`Up/${question.questionId}`}
-          disabled={questionIndex === 0}
-          onClick={handleOnUpDownButtonClick}
-          aria-label={`Move question ${question.fieldTitle} up`}
-        >
-          Up
-        </button>
-        <button
-          className={`govuk-button govuk-button--secondary govuk-!-margin-right-2 govuk-!-margin-bottom-0 ${styles['button']}`}
-          data-module="govuk-button"
-          data-cy="cyDownButton"
-          name={`Down/${question.questionId}`}
-          disabled={questionIndex === questions.length - 1}
-          onClick={handleOnUpDownButtonClick}
-          aria-label={`Move question ${question.fieldTitle} down`}
-        >
-          Down
-        </button>
+      <button
+        className={`govuk-button govuk-button--secondary govuk-!-margin-right-2 govuk-!-margin-bottom-0 ${styles['button']}`}
+        data-module="govuk-button"
+        data-cy="cyUpButton"
+        name={`Up/${question.questionId}`}
+        disabled={questionIndex === 0}
+        onClick={handleOnUpDownButtonClick}
+        aria-label={`Move question ${question.fieldTitle} up`}
+      >
+        Up
+      </button>
+      <button
+        className={`govuk-button govuk-button--secondary govuk-!-margin-right-2 govuk-!-margin-bottom-0 ${styles['button']}`}
+        data-module="govuk-button"
+        data-cy="cyDownButton"
+        name={`Down/${question.questionId}`}
+        disabled={questionIndex === questions.length - 1}
+        onClick={handleOnUpDownButtonClick}
+        aria-label={`Move question ${question.fieldTitle} down`}
+      >
+        Down
+      </button>
 
-        <CustomLink
-          href={`/build-application/${applicationId}/${section.sectionId}/${question.questionId}/edit/question-content`}
-          customStyle={`${styles['wrap']}`}
-        >
-          Edit
-        </CustomLink>
-      </div>
+      <CustomLink
+        href={`/build-application/${applicationId}/${section.sectionId}/${question.questionId}/edit/question-content`}
+        customStyle={`${styles['wrap']}`}
+      >
+        Edit
+      </CustomLink>
     </div>
   );
 };
