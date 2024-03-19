47079d60ba6dd8d8ff6c831c4ea35f4040419508
rachelswart <99667350+rachelswart@users.noreply.github.com>
2024-03-19 12:36:46 +0000

    TMI2-720: view submissions link when logged out (#404)
    * adds redirect url
    
    * fixed test
    
    * pr comments

--- packages/admin/src/middleware.page.ts
+++ packages/admin/src/middleware.page.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line  @next/next/no-server-import-in-page
-import { NextRequest, NextResponse } from 'next/server';
-import { getLoginUrl } from './utils/general';
+import { NextRequest, NextResponse, URLPattern } from 'next/server';
 import { isAdminSessionValid } from './services/UserService';
 import { csrfMiddleware } from './utils/csrfMiddleware';
+import { getLoginUrl } from './utils/general';
 
 // It will apply the middleware to all those paths
 // (if new folders at page root are created, they need to be included here)
@@ -54,9 +54,15 @@ export async function middleware(req: NextRequest) {
     });
 
     res.headers.set('Cache-Control', 'no-store');
-
     return res;
-  } else {
-    return NextResponse.redirect(getLoginUrl());
   }
+  let url = getLoginUrl();
+  if (submissionDownloadPattern.test({ pathname: req.nextUrl.pathname })) {
+    url = url + req.nextUrl.pathname;
+  }
+  return NextResponse.redirect(url);
 }
+
+const submissionDownloadPattern = new URLPattern({
+  pathname: '/scheme/:schemeId([0-9]+)/:exportBatchId([0-9]+)',
+});
