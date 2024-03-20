c4d3b85acc874ce0dda695b939e57311616370d8
Conor Fayle <141320269+ConorFayleTCO@users.noreply.github.com>
2024-03-20 10:11:21 +0000

    GAP-2517: put application name into the downloaded application form summary odt (#411)
    * GAP-2517 | put application name into the downloaded odt
    
    * GAP-2517 | formatting and linting
    
    ---------
    
    Co-authored-by: conor <conor.fayle@and.digital>

--- packages/admin/src/pages/api/applications/[applicationId]/download-summary.page.tsx
+++ packages/admin/src/pages/api/applications/[applicationId]/download-summary.page.tsx
@@ -1,17 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { downloadSummary } from '../../../../services/ApplicationService';
+import {
+  downloadSummary,
+  getApplicationFormSummary,
+} from '../../../../services/ApplicationService';
 import { getSessionIdFromCookies } from '../../../../utils/session';
 import { APIGlobalHandler } from '../../../../utils/apiErrorHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const sessionCookie = getSessionIdFromCookies(req);
   const applicationId = (req.query.applicationId || '').toString();
-  const { data } = await downloadSummary(
+
+  const { data } = await downloadSummary(applicationId, sessionCookie);
+
+  const application = await getApplicationFormSummary(
     applicationId,
-    getSessionIdFromCookies(req)
+    sessionCookie,
+    false,
+    false
   );
+  const applicationName = application.applicationName
+    .substring(0, 100)
+    .replaceAll(new RegExp('[^a-zA-Z0-9\\-\\.()]', 'g'), '_');
+  const filename = `${applicationName}_questions.odt`;
 
   res.setHeader('Content-Type', 'application/octet-stream');
-  res.setHeader('Content-Disposition', 'attachment; filename=application.odt');
+  res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
   res.send(Buffer.from(data));
 }
 
