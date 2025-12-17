f5f9d4d5778308b4bbb2e6a33380de61d5b08d1f
Richard Mohammed <Richard.Mohammed@goaco.com>
2025-12-17 14:52:34 +0000

    Update hint text for application name submission to clarify the allowance of multiple applications.

--- packages/applicant/src/pages/applications/[applicationId]/name-submission.page.tsx
+++ packages/applicant/src/pages/applications/[applicationId]/name-submission.page.tsx
@@ -153,7 +153,7 @@ export default function NameSubmissionPage({
             questionTitle="Name this application"
             questionHintText={
               <>
-                You have already applied for this grant. Give this application a different name so you can tell it apart from your other applications.
+                This grant allows multiple applications. Give this application a name so you can tell it apart from other applications.
                 <br />
                 <br />
                 Your application name must only use letters and numbers. For example, &apos;Application 2&apos; or &apos;Smith Family&apos;.
